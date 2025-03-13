6074b04e4b3d1b0dfe5733d5f984235854baccc7
Yoann Saget <yoasaget@gmail.com>
2025-03-13 16:39:35 +0100

    add menu to all pages

--- apps/a11y-front/src/app/pages/contact-page/contact-page.component.ts
+++ apps/a11y-front/src/app/pages/contact-page/contact-page.component.ts
@@ -1,16 +1,17 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from "@angular/forms";
-import {Router} from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { HeaderComponent } from "../../components/header/header.component";
 
 export class ContactFormData {
-  constructor(public name: string, public age: number, public email: string, public phoneNumber: string, public message: string) {}
+  constructor(public name: string, public age: number, public email: string, public phoneNumber: string, public message: string) { }
 }
 
 @Component({
   selector: 'a11y-contact-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HeaderComponent],
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss'],
 })
--- apps/a11y-front/src/app/pages/not-allowed-logged-out-page/not-allowed-logged-out-page.component.ts
+++ apps/a11y-front/src/app/pages/not-allowed-logged-out-page/not-allowed-logged-out-page.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterLink} from "@angular/router";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'a11y-not-allowed-logged-out-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HeaderComponent],
   templateUrl: './not-allowed-logged-out-page.component.html',
   styleUrls: ['./not-allowed-logged-out-page.component.scss'],
 })
